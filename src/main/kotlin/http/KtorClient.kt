fcd424f2a87486367ee733a891a68e1d385b1858
himitery <himit0131@gmail.com>
2022-04-19 14:12:13 +0900

    fix(http): fix ktorClient default request url

--- src/main/kotlin/http/KtorClient.kt
+++ src/main/kotlin/http/KtorClient.kt
@@ -6,14 +6,13 @@ import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
-import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 
 fun getKtorClient(notionApiKey: String): HttpClient {
 	return HttpClient {
 		defaultRequest {
-			url(URLBuilder("https://api.notion.com/v1/").buildString())
+			url("https://api.notion.com/")
 			headers {
 				header("Accept", "application/json")
 				header("Notion-Version", Config.notionVersion)
