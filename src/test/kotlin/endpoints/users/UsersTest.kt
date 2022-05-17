365a57ba6d7316f63f827044e402beacc89bd7a6
himitery <himit0131@gmail.com>
2022-05-17 10:49:11 +0900

    test(user): extends kotest annotation spec

--- src/test/kotlin/endpoints/users/UsersTest.kt
+++ src/test/kotlin/endpoints/users/UsersTest.kt
@@ -4,33 +4,33 @@ import Config
 import endpoints.users.dto.response.UserListResponse
 import endpoints.users.dto.response.UserResponse
 import http.getKtorClient
+import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.matchers.shouldNotBe
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
 
-internal class UsersTest {
-    private val users: Users = Users(getKtorClient(Config.notionApiKey))
+internal class UsersTest : AnnotationSpec() {
+	private val users: Users = Users(getKtorClient(Config.notionApiKey))
 
-    @Test
-    @DisplayName("[Users] List all users")
-    fun getUserList() {
-        val result = runBlocking {
-            users.list()
-        }
+	@Test
+	@DisplayName("[Users] List all users")
+	fun getUserList() {
+		val result = runBlocking {
+			users.list()
+		}
 
-        result shouldNotBe null
-        result is UserListResponse
-    }
+		result shouldNotBe null
+		result is UserListResponse
+	}
 
-    @Test
-    @DisplayName("[Users] Retrieve your token's bot user")
-    fun getUserMe() {
-        val result = runBlocking {
-            users.me()
-        }
+	@Test
+	@DisplayName("[Users] Retrieve your token's bot user")
+	fun getUserMe() {
+		val result = runBlocking {
+			users.me()
+		}
 
-        result shouldNotBe null
-        result is UserResponse
-    }
+		result shouldNotBe null
+		result is UserResponse
+	}
 }
